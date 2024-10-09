b47d66b63f673fd09facec7ffc57615e8bb1c4b1
MagyarMark <magyar.mark.jozsef@diak.szbi-pg.hu>
2024-10-09 09:44:46 +0200

    update test calculator

--- test_calculator.py
+++ test_calculator.py
@@ -14,3 +14,17 @@ def test_substract():
     assert substract(20, 14) == 6
     assert substract(10, 14) == -4
     assert substract(0, 0) == 0
+    
+def test_multiply():
+    assert multiply(10, 5) == 50
+    assert multiply(1, 1) == 1
+    assert multiply(2, 1) == 2
+    assert multiply(8, 4) == 32
+    assert multiply(0, 0) == 0
+
+def test_divide():
+    assert divide(10, 5) == 2
+    assert divide(1, 1) == 1
+    assert divide(2, 1) == 2
+    assert divide(8, 4) == 2
+    assert divide(5, 5) == 1
